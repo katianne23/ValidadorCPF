6c3fa1ec31028eb4a32745089040746edcd2cd12
Katianne Araujo <71135012+katianne23@users.noreply.github.com>
2022-05-11 11:34:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,61 @@
-function validarCPF(cpf){
-    if(cpf.length != 11){
-        return false;
-    } else {
-        var numeros = cpf.substring(0, 9);
-        var digitos = cpf.substring(9);
-        
-        var soma =0;
-        for(var i = 10; i > 1; i--){
-            soma += numeros.charAt(10 - i) * i;
-        }
-        
+function validaCPF(cpf) {
+  var numeros, digitos, soma, i, resultado, digitos_iguais;
+  digitos_iguais = 1;
 
-        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+  if (cpf.length < 11) {
+    return false;
+  }
 
-        //Validaçao primeiro digito
-        if(resultado != digitos.charAt(0)){
-            return false;
-        }
-
-        soma = 0;
-        numeros = cpf.substring(0, 10);
-
-
-        //validaçao segundo digito
-        for(var k = 11; k > 1; k--){
-            soma += numeros.charAt(11 - k) * k;
-        }
-
-        resultado = soma % 11 > 2 ? 0 : 11 - (soma % 11);
-
-        if(resultado != digitos.charAt(1)){
-            return false;
-        }
-    
-        return true;
+  for (i = 0; i < cpf.length - 1; i++) {
+    if (cpf.charAt(i) != cpf.charAt(i + 1)) {
+      digitos_iguais = 0;
+      break;
     }
+  }
+
+  if (!digitos_iguais) {
+    numeros = cpf.substring(0, 9);
+    digitos = cpf.substring(9);
+    soma = 0;
+
+    for (i = 10; i > 1; i--) {
+      soma += numeros.charAt(10 - i) * i;
+    }
+
+    resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+    if (resultado != digitos.charAt(0)) {
+      return false;
+    }
+
+    numeros = cpf.substring(0, 10);
+    soma = 0;
+
+    for (i = 11; i > 1; i--) {
+      soma += numeros.charAt(11 - i) * i;
+    }
+
+    resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+    if (resultado != digitos.charAt(1)) {
+      return false;
+    }
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function validacao(){
-   
-    document.getElementById('success').style.display = 'none';
-    document.getElementById('error').style.display = 'none';
-    
-    var cpf = document.getElementById('cpf_digitado').value;
-    
-    var resultadoValidacao = validarCPF(cpf);
+function validacao() {
+  console.log('Iniciando validação CPF');
+  document.getElementById('success').style.display = 'none';
+  document.getElementById('error').style.display = 'none';
 
-      if(resultadoValidacao){
-        document.getElementById('success').style.display = 'block';
-      } else {
-        document.getElementById('error').style.display = 'block';
-      }
+  var cpf = document.getElementById('cpf_digitado').value;
+
+  if (validaCPF(cpf)) {
+    document.getElementById('success').style.display = 'block';
+  } else {
+    document.getElementById('error').style.display = 'block';
+  }
+  console.log('Finalizando validação CPF');
 }
